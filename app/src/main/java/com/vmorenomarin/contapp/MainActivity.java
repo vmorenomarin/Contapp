4ce213d96da52147a499806d48a442fb24bcea7e
vmorenomarin <vmorenomarin@gmail.com>
2016-08-01 17:39:28 -0500

    Se agregan campos de formulario al archivo activity_main.xml

--- app/src/main/java/com/vmorenomarin/contapp/MainActivity.java
+++ app/src/main/java/com/vmorenomarin/contapp/MainActivity.java
@@ -2,8 +2,22 @@ package com.vmorenomarin.contapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+//import android.widget.EditText;
+import android.support.design.widget.TextInputEditText;
+import android.text.Editable;
+import android.widget.Toast;
+import android.support.design.widget.TextInputLayout;
+import android.text.TextUtils;
+import android.text.TextWatcher;
+import android.view.View;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextInputEditText inputNameEditable, inputEmailEditable, inputPhoneEditable, inputDatebirthdayEditable, inputDescriptionEditable;
+    private TextInputLayout inputLayoutName, inputLayoutEmail, inputLayoutPhone, inputLayoutDatebirthday, inputLayoutDescription;
+    private Button btnConformation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
