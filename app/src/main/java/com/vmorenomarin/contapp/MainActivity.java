0cd49414d3e9bfe6dd0418e1b29b8921cc6f7bda
vmorenomarin <vmorenomarin@gmail.com>
2016-08-01 18:55:34 -0500

    Se definen los objetos asociados a los campos del archivo activity_main.

--- app/src/main/java/com/vmorenomarin/contapp/MainActivity.java
+++ app/src/main/java/com/vmorenomarin/contapp/MainActivity.java
@@ -9,19 +9,42 @@ import android.text.Editable;
 import android.widget.Toast;
 import android.support.design.widget.TextInputLayout;
 import android.text.TextUtils;
-import android.text.TextWatcher;
+import android.text.TextWatcher;  //valida el texto ingresado.
 import android.view.View;
 import android.view.WindowManager;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextInputEditText inputNameEditable, inputEmailEditable, inputPhoneEditable, inputDatebirthdayEditable, inputDescriptionEditable;
     private TextInputLayout inputLayoutName, inputLayoutEmail, inputLayoutPhone, inputLayoutDatebirthday, inputLayoutDescription;
-    private Button btnConformation;
+    private Button btnConfirmation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        inputLayoutName     = (TextInputLayout) findViewById(R.id.inf);
+        inputLayoutDatebirthday =   (TextInputLayout) findViewById(R.id.idf);
+        inputLayoutPhone    = (TextInputLayout) findViewById(R.id.ipf);
+        inputLayoutEmail    = (TextInputLayout) findViewById(R.id.ief);
+        inputLayoutDescription  = (TextInputLayout) findViewById(R.id.idf);
+
+        inputNameEditable   = (TextInputEditText) findViewById(R.id.ine);
+        inputDatebirthdayEditable   =   (TextInputEditText) findViewById(R.id.ibe);
+        inputPhoneEditable  =   (TextInputEditText) findViewById(R.id.ipe);
+        inputEmailEditable  =   (TextInputEditText) findViewById(R.id.iee);
+        inputDescriptionEditable    =   (TextInputEditText) findViewById(R.id.ide);
+
+        btnConfirmation =   (Button) findViewById(R.id.btnConfirmation);
+
+
+
+
+
     }
+
+
 }
